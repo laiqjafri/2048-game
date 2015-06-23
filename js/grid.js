bf1d1134be941e98180486a4f095b8c2cca49aae
Laiq Jafri <laiq.jafri@gmail.com>
2015-06-24 00:37:28 +0500

    Method for setting/initializing random empty cells

--- js/grid.js
+++ js/grid.js
@@ -16,3 +16,23 @@ Grid.prototype.print = function() {
     console.log(values.join("\t"));
   }
 }
+
+Grid.prototype.set_random_cells = function(number_of_cells) {
+  for(i=0; i<number_of_cells; i++) {
+    var empty_cells = this.empty_cells(); //calling it inside the loop removes the chance of setting the same cell again
+    var random = Math.floor(Math.random() * (empty_cells.length + 1));
+    empty_cells[random].value = 2;
+  }
+}
+
+Grid.prototype.empty_cells = function() {
+  var empty_cells = [];
+  for(x=0; x<this.size; x++) {
+    for(y=0; y<this.size; y++) {
+      if(this.cells[x][y].value == 0) {
+        empty_cells.push(this.cells[x][y]);
+      }
+    }
+  }
+  return empty_cells;
+}
