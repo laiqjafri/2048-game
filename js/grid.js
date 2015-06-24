eb331e89aac905dd69130fe1b97b7af29970c09e
Laiq Jafri <laiq.jafri@gmail.com>
2015-06-25 00:59:27 +0500

    Some style to the grid

--- js/grid.js
+++ js/grid.js
@@ -119,13 +119,34 @@ Grid.prototype.move = function(direction) {
 Grid.prototype.render_html = function() {
   var html = "";
   for(y=0; y<this.size; y++) {
-    var row = "<tr>";
+    var row = "<div class='row text-center'>";
     for(x=0; x<this.size; x++) {
-      row += ("<td>" + this.cells[x][y].value + "</td>");
+      if(this.cells[x][y].value) {
+        row += ("<div class='col-xs-1 tile' data-number='" + this.cells[x][y].value + "'>" + this.cells[x][y].value + "</div>");
+      } else {
+        row += ("<div class='col-xs-1 tile' data-number='0'>&nbsp</div>");
+      }
     }
-    row += "</tr>";
+    row += "</div>";
     html += row;
     $("#grid").html(html);
     $("#score").html(this.score);
+    var colors = {
+      '0'    : '#FFFAFA',
+      '2'    : '#FFFAF0',
+      '4'    : '#FFFACD',
+      '8'    : '#FA8072',
+      '16'   : '#F4A460',
+      '32'   : '#F08080',
+      '64'   : '#E9967A',
+      '128'  : '#DAA520',
+      '256'  : '#D2B48C',
+      '512'  : '#D2691E',
+      '1024' : '#B8860B',
+      '2048' : '#B22222',
+    };
+    $(".tile").each(function(index, tile) {
+      $(tile).css('background-color', colors[$(tile).data('number')])
+    });
   }
 }
