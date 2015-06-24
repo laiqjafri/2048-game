a02d71a2169a04912da94a57ff2ba62606dc4d27
Laiq Jafri <laiq.jafri@gmail.com>
2015-06-24 15:28:50 +0500

    Left merge algo. TODO: refactor/generalize this

--- js/grid.js
+++ js/grid.js
@@ -20,8 +20,10 @@ Grid.prototype.print = function() {
 Grid.prototype.set_random_cells = function(number_of_cells) {
   for(i=0; i<number_of_cells; i++) {
     var empty_cells = this.empty_cells(); //calling it inside the loop removes the chance of setting the same cell again
-    var random = Math.floor(Math.random() * (empty_cells.length + 1));
-    empty_cells[random].value = 2;
+    if(empty_cells.length) {
+      var random = Math.floor(Math.random() * empty_cells.length);
+      empty_cells[random].value = 2;
+    }
   }
 }
 
@@ -35,4 +37,52 @@ Grid.prototype.empty_cells = function() {
     }
   }
   return empty_cells;
+}
+
+Grid.prototype.merge = function(direction) {
+  var merge = false;
+  if(direction == "left") {
+    for(y=0; y<this.size; y++) {
+      for(x=0; x<this.size; x++) {
+        var found = false;
+        var next  = x + 1;
+        while(next < this.size && !found) {
+          if(typeof(this.cells[next]) != "undefined" && this.cells[next][y].value) {
+            found = true;
+          } else {
+            next++;
+          }
+        }
+        if(this.cells[x][y].value) {
+          if(found) {
+            if(this.cells[x][y].value == this.cells[next][y].value) {
+              this.cells[x][y].value    *= 2;
+              this.cells[next][y].value  = 0;
+              merge = true;
+            } else {
+              if(next != (x+1)) {
+                this.cells[x+1][y].value  = this.cells[next][y].value;
+                this.cells[next][y].value = 0;
+                merge = true;
+              }
+            }
+          }
+        } else {
+          if(found) {
+            this.cells[x][y].value    = this.cells[next][y].value;
+            this.cells[next][y].value = 0;
+            x -= 1;
+            merge = true;
+          }
+        }
+      }
+    }
+  } else if(direction == "right") {
+  } else if(direction == "up") {
+  } else if(direction == "down") {
+  }
+  if(merge) {
+    this.set_random_cells(1);
+  }
+  this.print();
 }
