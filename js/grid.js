65b1342d9a7ae1626b712c763a9dd6ca678972e1
Laiq Jafri <laiq.jafri@gmail.com>
2015-06-24 20:30:49 +0500

    Refactored/Generalized the merge functionality

--- js/grid.js
+++ js/grid.js
@@ -43,49 +43,105 @@ Grid.prototype.empty_cells = function() {
   return empty_cells;
 }
 
-Grid.prototype.merge = function(direction) {
-  var merge = false;
-  if(direction == "left") {
-    for(y=0; y<this.size; y++) {
-      for(x=0; x<this.size; x++) {
-        var found = false;
-        var next  = x + 1;
-        while(next < this.size && !found) {
-          if(typeof(this.cells[next]) != "undefined" && this.cells[next][y].value) {
-            found = true;
-          } else {
-            next++;
-          }
-        }
-        if(this.cells[x][y].value) {
-          if(found) {
-            if(this.cells[x][y].value == this.cells[next][y].value) {
-              this.cells[x][y].value    *= 2;
-              this.cells[next][y].value  = 0;
-              merge = true;
-            } else {
-              if(next != (x+1)) {
-                this.cells[x+1][y].value  = this.cells[next][y].value;
-                this.cells[next][y].value = 0;
-                merge = true;
-              }
-            }
-          }
-        } else {
-          if(found) {
-            this.cells[x][y].value    = this.cells[next][y].value;
-            this.cells[next][y].value = 0;
-            x -= 1;
-            merge = true;
-          }
+Grid.prototype.next_mergeable_cell = function(x, y, horizontal, step) {
+  var found    = false;
+  var next     = horizontal ? (x + step) : (y + step);
+  var boundary = step > 0 ? this.size : -1;
+
+  //Find the next non-empty cell
+  while(next != boundary && !found) {
+    var point = horizontal ? {x:next, y:y} : {x:x, y:next};
+    if(typeof(this.cells[point.x]) != "undefined" &&
+        typeof(this.cells[point.x][point.y]) != "undefined" &&
+        this.cells[point.x][point.y].value) {
+      found = true;
+    } else {
+      next = horizontal ? (next + step) : (next + step);
+    }
+  }
+  return next;
+}
+
+Grid.prototype.merge = function(x, y, horizontal, step) {
+  var next           = this.next_mergeable_cell(x, y, horizontal, step);
+  var point          = horizontal ? {x:next, y:y} : {x:x, y:next};
+  var neighbor_point = horizontal ? {x:(x + step), y:y} : {x:x, y:(y + step)};
+  if(this.cells[x][y].value) {
+    if(next < this.size && next > -1) {//Found a cell to merge
+      if(this.cells[x][y].value == this.cells[point.x][point.y].value) {
+        //if values are same, merge them into one cell and double the value
+        this.cells[x][y].value              *= 2;
+        this.cells[point.x][point.y].value   = 0;
+        return {merged: true, increment: 1};
+      } else {
+        //otherwise move them next to each other
+        if(!(point.x == neighbor_point.x && point.y == neighbor_point.y)) {
+          this.cells[neighbor_point.x][neighbor_point.y].value = this.cells[point.x][point.y].value;
+          this.cells[point.x][point.y].value = 0;
+          return {merged: true, increment: 1};
         }
       }
     }
-  } else if(direction == "right") {
-  } else if(direction == "up") {
-  } else if(direction == "down") {
+  } else {
+    //move the found cell farthest
+    if(next < this.size && next > -1) {
+      this.cells[x][y].value             = this.cells[point.x][point.y].value;
+      this.cells[point.x][point.y].value = 0;
+      return {merged: true, increment: 0};
+    }
   }
-  if(merge) {
+  return {merged: false, increment: 1};
+}
+
+Grid.prototype.move = function(direction) {
+  var merged = false;
+  if(direction == "left") {
+    var horizontal = true;
+    var step = 1;
+    for(y=0; y<this.size; y++) {
+      var x = 0;
+      while(x < this.size) {
+        var merge_action = this.merge(x, y, horizontal, step);
+        x += merge_action.increment;
+        merged = merged || merge_action.merged;
+      }
+    }
+  } else if(direction == "right") {
+    var horizontal = true;
+    var step = -1;
+    for(y=0; y<this.size; y++) {
+      var x = this.size - 1;
+      while(x >= 0) {
+        var merge_action = this.merge(x, y, horizontal, step);
+        x -= merge_action.increment;
+        merged = merged || merge_action.merged;
+      }
+    }
+  } else if(direction == "up") {
+    var horizontal = false;
+    var step = 1;
+    for(x=0; x<this.size; x++) {
+      var y = 0;
+      while(y < this.size) {
+        var merge_action = this.merge(x, y, horizontal, step);
+        y += merge_action.increment;
+        merged = merged || merge_action.merged;
+      }
+    }
+  } else if(direction == "down") {
+    var horizontal = false;
+    var step = -1;
+    for(x=0; x<this.size; x++) {
+      var y = this.size - 1;
+      while(y >= 0) {
+        var merge_action = this.merge(x, y, horizontal, step);
+        y -= merge_action.increment;
+        merged = merged || merge_action.merged;
+      }
+    }
+  }
+
+  if(merged) {
     this.set_random_cells(1);
   }
   this.print();
